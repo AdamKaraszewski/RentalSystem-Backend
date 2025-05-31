e6c4ae63ce346bd1e5b3fb53e53047e7d002e304
Adam Karaszewski <karaszewskiadam@outlook.com>
2025-05-31 15:52:15 +0200

    Created Spring Boot web app using Spring Initializr

---
+++ backend/roomie/src/test/java/com/rental_manager/roomie/RoomieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rental_manager.roomie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
