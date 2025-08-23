aac39f0f6239bea55dd742a7333a4aee5071ec0e
Adam Karaszewski <karaszewskiadam@outlook.com>
2025-08-23 20:51:59 +0200

    spring security dependency was added to pom.xml. Simple SecurityConfig class was implemented - all endpoints are unsecured (permit all)

---
+++ backend/src/main/java/com/rental_manager/roomie/config/security/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.rental_manager.roomie.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated())
+                .build();
+    }
+}
